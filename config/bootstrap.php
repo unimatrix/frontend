7becd352f6e4cc16876f52e12af37af94aed6959
Highstrike <highstrike@gmail.com>
2017-09-21 17:39:57 +0300

    skip debug_kit

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,9 +4,10 @@ use Cake\Core\Configure;
 use Cake\Event\EventManager;
 use Cake\Http\Middleware\CsrfProtectionMiddleware;
 
-// is backend? don't continue
-if(Configure::read('Backend') && explode('/', env('REQUEST_URI'))[1] === 'backend')
-    return;
+// is backend or debug_kit? don't continue
+if(Configure::read('Backend') && explode('/', env('REQUEST_URI'))[1] === 'backend'
+    || explode('/', env('REQUEST_URI'))[1] === 'debug_kit')
+        return;
 
 // attach middleware
 EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
