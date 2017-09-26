ac6e9645ef8553effe344b4121c20a5132e506f0
Highstrike <highstrike@gmail.com>
2017-09-26 12:37:59 +0300

    remove middleware from cake

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
+use Cake\Core\Plugin;
 use Cake\Core\Configure;
 use Cake\Event\EventManager;
 use Cake\Http\Middleware\CsrfProtectionMiddleware;
+use Unimatrix\Cake\Error\Middleware\EmailErrorHandlerMiddleware;
+
+// load Unimatrix Cake
+Plugin::load('Unimatrix/Cake');
 
 // is cli or backend or debug_kit? don't continue
 if(PHP_SAPI === 'cli' || Configure::read('Backend') && explode('/', env('REQUEST_URI'))[1] === 'backend'
@@ -11,6 +16,9 @@ if(PHP_SAPI === 'cli' || Configure::read('Backend') && explode('/', env('REQUEST
 
 // attach middleware
 EventManager::instance()->on('Server.buildMiddleware', function ($event, $queue) {
+    // EmailErrorHandlerMiddleware
+    $queue->insertAt(0, EmailErrorHandlerMiddleware::class);
+
     // CsrfProtectionMiddleware
     if(Configure::read('Frontend.security.enabled'))
         $queue->add(new CsrfProtectionMiddleware([
